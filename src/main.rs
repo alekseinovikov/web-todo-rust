5e5b77d6ebf37ac22bb9c838db9645ecf4619c6f
alekseinovikov <novikov.aleksei.92@gmail.com>
2024-04-07 18:14:54 +0300

    Added htmx support added embed fs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+use rocket::fs::{FileServer, NamedFile};
+use rocket::response::content;
+use rust_embed::RustEmbed;
+
+#[derive(RustEmbed)]
+#[folder = "static/components/"]
+struct Components;
+
+#[derive(RustEmbed)]
+#[folder = "static/public/"]
+struct Public;
+
+#[get("/api/todos")]
+async fn todo_list() -> content::RawHtml<String> {
+    let content = Components::get("todo-list.html").unwrap();
+    let string = String::from_utf8(content.data.into_owned()).unwrap();
+    content::RawHtml(string)
+}
+
+#[get("/")]
+async fn index() -> Option<NamedFile> {
+    NamedFile::open("static/public/index.html").await.ok()
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index, todo_list])
+        .mount("/", FileServer::from("static/public"))
 }
