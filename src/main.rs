d2a9c27d3aa148b94d32bdc08af92de046f42f11
alekseinovikov <novikov.aleksei.92@gmail.com>
2024-04-08 15:58:37 +0300

    Added embedded static - public\private files

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 #[macro_use]
 extern crate rocket;
 
+use std::path::PathBuf;
+
+use rocket::{Request, response};
 use rocket::fs::{FileServer, NamedFile};
-use rocket::response::content;
+use rocket::http::{ContentType, Status};
+use rocket::http::uri::Segments;
+use rocket::response::{content, Responder};
 use rust_embed::RustEmbed;
 
 #[derive(RustEmbed)]
@@ -20,14 +25,47 @@ async fn todo_list() -> content::RawHtml<String> {
     content::RawHtml(string)
 }
 
+struct EmbeddedFile {
+    data: Vec<u8>,
+    content_type: ContentType,
+}
+
+impl<'r> Responder<'r, 'static> for EmbeddedFile {
+    fn respond_to(self, _: &'r Request<'_>) -> response::Result<'static> {
+        response::Response::build()
+            .sized_body(self.data.len(), std::io::Cursor::new(self.data))
+            .header(self.content_type)
+            .ok()
+    }
+}
+
 #[get("/")]
-async fn index() -> Option<NamedFile> {
-    NamedFile::open("static/public/index.html").await.ok()
+async fn index() -> content::RawHtml<String> {
+    let content = Public::get("index.html").unwrap();
+    let string = String::from_utf8(content.data.into_owned()).unwrap();
+    content::RawHtml(string)
+}
+
+#[get("/<file..>")]
+fn public_files(file: std::path::PathBuf) -> Result<EmbeddedFile, Status> {
+    let path = file.to_str().unwrap_or_default();
+    match Public::get(path) {
+        Some(content) => {
+            let content_type = ContentType::from_extension(
+                file.extension().and_then(|ext| ext.to_str()).unwrap_or_default()
+            ).unwrap_or(ContentType::Binary);
+
+            Ok(EmbeddedFile {
+                data: content.data.into_owned(),
+                content_type,
+            })
+        }
+        None => Err(Status::NotFound),
+    }
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index, todo_list])
-        .mount("/", FileServer::from("static/public"))
+        .mount("/", routes![index, public_files, todo_list])
 }
